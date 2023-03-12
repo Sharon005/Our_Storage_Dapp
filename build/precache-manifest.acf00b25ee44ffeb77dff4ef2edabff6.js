46e2e5a22602462e90c365c68a8dc2ea23b8c948
Sharon005 <69362399+Sharon005@users.noreply.github.com>
2023-03-12 07:14:35 +0530

    Add files via upload

---
+++ build/precache-manifest.acf00b25ee44ffeb77dff4ef2edabff6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a81f5177903ae5ed7f0",
+    "url": "/Our_Storage_Dapp/static/css/main.a9d0e6a3.chunk.css"
+  },
+  {
+    "revision": "7a81f5177903ae5ed7f0",
+    "url": "/Our_Storage_Dapp/static/js/main.7a81f517.chunk.js"
+  },
+  {
+    "revision": "b17509c6619fcf83438b",
+    "url": "/Our_Storage_Dapp/static/css/1.df09ca28.chunk.css"
+  },
+  {
+    "revision": "b17509c6619fcf83438b",
+    "url": "/Our_Storage_Dapp/static/js/1.b17509c6.chunk.js"
+  },
+  {
+    "revision": "cad83c7928aa0880adc1",
+    "url": "/Our_Storage_Dapp/static/js/runtime~main.cad83c79.js"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/Our_Storage_Dapp/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/Our_Storage_Dapp/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "48ccf2593951b53b8803e1c322b9396d",
+    "url": "/Our_Storage_Dapp/index.html"
+  }
+];
